5c7500ca674bfa8034d6a6a7068fff899f8abfed
Zephrodite <zephexhkr51049@gmail.com>
2021-04-21 01:31:33 +0700

    del

--- LendyTryRun/LendyProj/app/src/main/java/com/example/lendyproj/model/Nickname.kt
+++
@@ -1,4 +0,0 @@
-package com.example.lendyproj.model
-
-data class Nickname(val en: String,
-                    val th: String)
--- LendyTryRun/LendyProj/app/src/main/java/com/example/lendyproj/ui/login/Book.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lendyproj.ui.login
-import com.google.firebase.database.Exclude
-import com.google.firebase.database.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class Book(val title: String? = null, val date: String? = null, val description: String? = null, val downloadUri: String? = null, @Exclude val key: String? = null) {
-}
