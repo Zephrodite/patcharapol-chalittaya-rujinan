ebfafa3e5d3321aa5e7e440214fd415492e58343
Zephrodite <zephexhkr51049@gmail.com>
2021-04-20 14:08:33 +0700

    commit hola

---
+++ LendyProj2/LendyProj/app/src/main/java/com/example/lendyproj/model/Nickname.kt
@@ -0,0 +1,4 @@
+package com.example.lendyproj.model
+
+data class Nickname(val en: String,
+                    val th: String)
---
+++ LendyProj2/LendyProj/app/src/main/java/com/example/lendyproj/ui/login/Book.kt
@@ -0,0 +1,7 @@
+package com.example.lendyproj.ui.login
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Book(val title: String? = null, val date: String? = null, val description: String? = null, val downloadUri: String? = null, @Exclude val key: String? = null) {
+}
