e1a517f9c5a77d31751bc87c44dc0c6e584d63a8
rujinan <rujinan@kkumail.com>
2021-04-22 21:15:04 +0700

    now

---
+++ LendyProj3/LendyProj/app/src/main/java/com/example/lendyproj/LendbookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lendyproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LendbookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lendbook)
+    }
+}
