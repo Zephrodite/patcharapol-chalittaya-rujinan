b1a5d0d676e208f2ad85d1c87476181a8ca7a187
rujinan <rujinan@kkumail.com>
2021-04-24 15:35:15 +0700

    profileupdate

---
+++ LendyProj3/LendyProj/app/src/main/java/com/example/lendyproj/WatchlistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lendyproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WatchlistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_watchlist)
+    }
+}
